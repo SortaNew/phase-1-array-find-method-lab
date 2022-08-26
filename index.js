fa62c9e45b3fa6bfb70f99341323bb4e7856bdea
Parry Huang <parryhuang94@gmail.com>
2022-08-26 11:49:09 -0400

    commit tree 7dca8ffec5b7f8815016209b9ff4d59dfa21488a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let won = record.find(array => array.result === "W")
+    if (won === undefined)
+        return won
+    else
+        return won.year
+}
+
+
